09a1141e195dfce39d79876cae7f9fd98b156adf
bdcontecha <bdcontecha@gmail.com>
2020-04-28 21:08:50 -0500

    final

--- principal.php
+++ principal.php
@@ -11,7 +11,7 @@
           if (mysqli_connect_errno($mysqli)) {
               echo "Fallo al conectar a MySQL: " . mysqli_connect_error();
           }
-          $sql = "Select * from empleado where usuario='$usuario' and contraseña='$clave'";
+          $sql = "Select * from usuario where nomb_usuario ='$usuario' and contrañ_usuario ='$clave'";
           $resultado = mysqli_query($mysqli, $sql);
           if (!$resultado) {
             echo "Fallo al ejecutar la consulta: (" . $mysqli->errno . ") " . $mysqli->error;
@@ -20,7 +20,7 @@
 
            
 
-            echo "bienvenido "
+            echo "bienvenido ";
           }
 ?>
 <html>
